2356509013cd9e4613b609e5fb92003346a18a8b
Laros <ing.arellano@gmail.com>
2018-12-15 16:25:31 +0100

    6.10. Criando filtro para CORS

---
+++ src/main/java/com/laros/api/cors/CorsFilter.java
@@ -0,0 +1,60 @@
+package com.laros.api.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/*
+ * Se implementa esto porque cors todavia funciona con spring security.
+ * */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter{
+
+	private String origenPermitido = "http://localhost"; //TODO: Configirar diferentes ambientes.
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		//Se envia en todas las solcitudes.
+		res.setHeader("Access-Control-Allow-Origin", origenPermitido);
+		res.setHeader("Access-Control-Allow-Credentials", "true"); // refresh
+		
+		if("OPTIONS".equals(req.getMethod()) && origenPermitido.equals(req.getHeader("Origin"))) {
+			
+			res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			res.setHeader("Access-Control-Max-Age", "3600");//1 hora tiempo que el buscador hace proxima solicitud.
+			
+			res.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, res);//Sigue flujo normal.
+		}
+	}
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+
+	@Override
+	public void destroy() {
+	}
+
+	
+}
--- src/main/java/com/laros/api/resource/CategoriaResource.java
+++ src/main/java/com/laros/api/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ public class CategoriaResource {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 	
+//	@CrossOrigin(maxAge = 10, origins = {"http://localhost"})
 	@GetMapping
 	public ResponseEntity<?> listar() {
 		List<Categoria> categorias = categoriaRepository.findAll();
