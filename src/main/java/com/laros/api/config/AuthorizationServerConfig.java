4dbf32832757f6a2ca320b40828f61e48df5627e
Laros <ing.arellano@gmail.com>
2018-12-23 18:31:39 +0100

    6.14. Implementando o logout

--- src/main/java/com/laros/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/laros/api/config/AuthorizationServerConfig.java
@@ -27,14 +27,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.secret("@ngul@r0")
 			.scopes("read","write")
 			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(40)
+			.accessTokenValiditySeconds(1800)//30 mins
 			.refreshTokenValiditySeconds(3600*24)
 		.and() 
 			.withClient("mobile")
 			.secret("m0b1l30")
 			.scopes("read")
 			.authorizedGrantTypes("password", "refresh_token")
-			.accessTokenValiditySeconds(40)
+			.accessTokenValiditySeconds(1800)
 			.refreshTokenValiditySeconds(3600*24)
 			;
 	}
---
+++ src/main/java/com/laros/api/resource/TokenResource.java
@@ -0,0 +1,30 @@
+package com.laros.api.resource;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Logout que elimina el token (refreshToken).
+ * */
+@RestController
+@RequestMapping("tokens")
+public class TokenResource {
+	
+	@DeleteMapping("revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse res) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(false);//TODO: En producción sera TRUE
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		res.addCookie(cookie);
+		res.setStatus(HttpStatus.NO_CONTENT.value());
+	}
+}
