e6154d7ddd6eca397d5979663931af668cbf3360
Laros <ing.arellano@gmail.com>
2020-04-19 14:10:55 +0200

    22.17. Enviando um e-mail simples

---
+++ src/main/java/com/laros/api/mail/Mailer.java
@@ -0,0 +1,52 @@
+package com.laros.api.mail;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+/*
+ * 22.17. Enviando um e-mail simples
+ * */
+@Component
+public class Mailer {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	/*
+	 * Escuchador para enviar.
+	 * */
+	@EventListener
+	public void test(ApplicationReadyEvent event) {
+		enviarEmail("testes.algaworks@gmail.com", 
+				Arrays.asList("ing.arellano@gmail.com"), 
+				"[larosproject] Probando envio",
+				"<br>Hola, ¿cómo estas?</br>");
+		
+		System.out.println("[Mailer.test]---------->Enviando Email...");
+	}
+	
+	public void enviarEmail(String remitente, List<String> destinatarios, String asunto ,String mensaje) {
+		try {
+			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+			helper.setFrom(remitente);
+			helper.setTo(destinatarios.toArray(new String[destinatarios.size()]));
+			helper.setSubject(asunto);
+			helper.setText(mensaje, true);			
+			
+			javaMailSender.send(mimeMessage);
+		} catch (MessagingException e) {
+			throw new RuntimeException("Problemas al enviar el email!", e);
+		}
+	}
+}
