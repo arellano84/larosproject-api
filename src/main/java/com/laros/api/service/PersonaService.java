ed6603d7bdc59c374b81ad09dfcf854184a9ab6e
Laros <ing.arellano@gmail.com>
2020-04-20 22:14:19 +0200

    22.25. Inserindo uma pessoa com contato

--- src/main/java/com/laros/api/service/PersonaService.java
+++ src/main/java/com/laros/api/service/PersonaService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.laros.api.mail.Mailer;
 import com.laros.api.model.Persona;
 import com.laros.api.repository.PersonaRepository;
 
